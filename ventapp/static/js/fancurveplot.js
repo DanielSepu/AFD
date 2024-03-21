36445b26721db20cc0937380143938c7cfc0970c
DuskyAlpaca <ravila@hibring.comq>
2024-03-21 17:00:50 -0300

    Bootstrap en local leido correctamente y mejora en responsividad

--- ventapp/static/js/fancurveplot.js
+++ ventapp/static/js/fancurveplot.js
@@ -2,7 +2,7 @@
       const margin = { top: 20, right: 30, bottom: 40, left: 40 };
       //const containerHeight = graphContainer.clientHeight;
 
-      function  createFanChart(data, chart_type, promedios, data2) {
+      function  createFanChart(data, chart_type, promedios, data2) { 
          // Get the dimensions of the container
          const containerWidth = graphContainer.clientWidth - margin.right;
          const containerHeight = graphContainer.clientHeight;
@@ -95,43 +95,44 @@
       }
 
       // Function to update chart dimensions based on container size
-      function updateFanChart(data) {
-         console.log("resize");
-
-         const keys = Object.keys(data[0]);
-         
+      function updateFanChart(data, chart_type, promedios, data2) {
          // Get the dimensions of the container
          const containerWidth = graphContainer.clientWidth - margin.right;
-         //const containerHeight = graphContainer.clientHeight;
-         const containerHeight = 500;
+         const containerHeight = graphContainer.clientHeight;
+         //const containerHeight = 500;
+         const keys = Object.keys(data[0]);
          // Declare the x (horizontal position) scale for "Q1".
-         const x = d3.scaleLinear()
-         .domain([0, 120])
-         .range([margin.left*2, containerWidth - margin.right]);
+            const x = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return d[keys[0]]; })])
+            .range([margin.left*2, containerWidth - margin.right]);
+
          // Declare the y (vertical position) scale for "Pt1".
-         const y = d3.scaleLinear()
-         .domain([0, d3.max(data, function(d) { return d[keys[1]]; })])
-         .range([containerHeight - margin.bottom, margin.top]);
-         // Remove any existing chart elements
-         d3.select("#graphContainer").selectAll("*").remove()
+            const y = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return d[keys[1]]; })])
+            .range([containerHeight - margin.bottom, margin.top]);
+
          // Create the SVG container.
          const svg = d3.create("svg")
-         .attr("width", containerWidth)
-         .attr("height", containerHeight)
+            .attr("width", containerWidth)
+            .attr("height", containerHeight);
+
          // Add the x-axis.
          svg.append("g")
          .attr("transform", `translate(0,${containerHeight - margin.bottom})`)
-         .call(d3.axisBottom(x))
+         .call(d3.axisBottom(x));
+
          // Add the y-axis.
          svg.append("g")
-            .attr("transform", `translate(${margin.left*2},0)`)
-            .call(d3.axisLeft(y))
+         .attr("transform", `translate(${margin.left*2},0)`)
+         .call(d3.axisLeft(y));
+
          // Add x-axis title.
          svg.append("text")
             .attr("x", containerWidth / 2)
             .attr("y", containerHeight )
             .style("text-anchor", "middle")
             .text(keys[0]);
+
          // Add y-axis title.
          svg.append("text")
             .attr("transform", "rotate(-90)")
@@ -139,14 +140,50 @@
             .attr("y", margin.left / 2)
             .style("text-anchor", "middle")
             .text(keys[1]);
+
          // Add the scatter plot points.
-         svg.selectAll("circle")
-         .data(data)
-         .enter().append("circle")
-         .attr("cx", function(d) { return x(d[keys[0]]); })
-         .attr("cy", function(d) { return y(d[keys[1]]); })
-         .attr("r", 5); // Tamaño de los punto
+            svg.selectAll("circle")
+            .data(data)
+            .enter().append("circle")
+            .attr("cx", function(d) { return x(d[keys[0]]); })
+            .attr("cy", function(d) { return y(d[keys[1]]); })
+            .attr("r", 5); // Tamaño de los puntos
+            
+         // Agregar punto de promedio  
+         // Valores de ejemplo 
+         svg.append("circle")
+         .attr("cx", x(promedios[0]))
+         .attr("cy", y(promedios[1]))
+         .attr("r", 8) 
+         .attr("fill", "red");
+      
+
+         // se ha agregado la linea   
+            svg.append("path")
+               .datum(data)
+               .attr("fill", "none")
+               .attr("stroke", "black")
+               .attr("stroke-width", 1.5)
+               .attr("d", d3.line()
+               .curve(d3.curveBasis) // Just add that to have a curve instead of segments
+               .x(function(d) { return x(d[keys[0]]); })
+               .y(function(d) { return y(d[keys[1]]); })
+            )
+            
+            svg.append("path")
+               .datum(data2)
+               .attr("fill", "none")
+               .attr("stroke", "black")
+               .attr("stroke-width", 1.5)
+               .attr("d", d3.line()
+               .curve(d3.curveBasis) // Just add that to have a curve instead of segments
+               .x(function(d) { return x(d[keys[0]]); })
+               .y(function(d) { return y(d[keys[1]]); })
+            )
+
+
          // Append the SVG element.
+         //graphContainer.innerHTML = ""; // Limpiar el contenedor antes de agregar el nuevo gráfico
          graphContainer.appendChild(svg.node());
       }
 

b26a6d119fd2100f53acd69319fea142b6dae489
zombozo <gerson.olivares543@gmail.com>
2024-11-22 22:36:07 -0600

    se agrego la gestion de error de calcular area del ducto

--- modules/semaforo.py
+++ modules/semaforo.py
@@ -146,6 +146,7 @@ class Semaforo:
         
         if area_ducto == None:
             raise ValueError("NO hemos calculado el area del ducto correctamente.")
+            messages.error(self.request, "Error algunos valores para calcular el area del ducto no se han especificado, verifique: tipo ducto: {self.project.ducto.t_ducto} y sus valores")
         return area_ducto
 
     def calcular_velocidad_sensor(self, tbs, hr, P, pt, ps, tipo):

e634d1e232149220cee34a33ea82f712ca09c921
zombozo <gerson.olivares543@gmail.com>
2025-07-09 14:35:18 -0600

    actualizacion de las vistas y el mixin de resultados

--- applications/fandesign/views.py
+++ applications/fandesign/views.py
@@ -271,6 +271,7 @@ class FanDesignView(FanCalculationsMixin, TemplateView):
             
             presion_dinamica = sensor_item.pt1 - sensor_item.ps1
             velocidad = velocidad_aire_sensor(presion_dinamica_sensor=presion_dinamica, densidad_aire_sensor1=densidad2)
+            
             caudal = caudal_aire_sensor1(velocidad_aire_sensor1=velocidad, area_ducto=proyecto.ducto.area) # type: ignore
 
             resistencia = self.compute_resistencia(ultima_med=ultima_med, caudal=caudal)
